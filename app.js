29f07637a4b72f5cdccb8a63d8b2e54c80a67bad
HafeezBaloch997 <hb583449@gmail.com>
2023-01-02 20:11:18 +0500

    javascript 9 to 11 chapter

---
+++ app.js
@@ -0,0 +1,74 @@
+//chapter no 6;
+
+//Qno #1;
+// document.write("Results:","<br>");
+// var num1 = 10;
+// document.write("The value of is: "+num1+ "<br>");
+// document.write("==========================","<br>","<br>");
+// num1++;
+// document.write("The value of ++a is: "+num1+"<br>");
+// document.write("Now the value of a is: "+num1+"<br>","<br>");
+// document.write("The value of a++ is: "+num1+"<br>");
+// ++num1;
+// document.write("Now the value of a++ is: "+num1+"<br>","<br>");
+
+// --num1;
+// document.write("The value of --a is: "+num1+"<br>");
+// document.write("Now the  value of a is: "+num1+"<br>","<br>");
+
+// document.write("The value of a-- is:"+num1+"<br>");
+// --num1;
+// document.write("Now the  value of a is: "+num1+"<br>");
+
+
+// Qno #2
+
+
+// var a = 2;
+// var b = 1;
+
+// a--;
+// document.write("a is " +a+"<br>");
+// --b;
+// document.write("b is "+b+"<br>");
+// var result = a-- - --b + ++b + b--;
+// document.write("Results is :" +result);
+
+// Qno #3
+
+
+// var name = prompt("Enter your name");
+// document.write("My name is " +name);
+
+
+// Qno #4;
+
+
+// var number = prompt("Enter your number ");
+// var result = number*5;
+// document.write("Multiply: "+result);
+
+
+// Qno 5
+
+// var sub_1 = prompt("Enter first subject name");
+// var subMarks1 = +prompt("Enter  first subect marks");
+// var sub_2 = prompt(" Enter 2nd subject name");
+// var subMarks2 = +prompt("Enter  2nd subect marks");
+// var sub_3 = prompt("Enter 3rd subject name");
+// var subMarks3 = +prompt("Enter  3rd subect marks");
+
+// var percentage1 = subMarks1 / marks  * 100; 
+// var percentage2 = subMarks2 / marks  * 100;
+// var percentage3 = subMarks3 / marks  * 100;
+
+// var marks = 100;
+
+// var total_marks = 300;
+// var obtainedMarks = subMarks1+subMarks2+subMarks3;
+// var percentage = obtainedMarks / total_marks * 100;
+
+// document.write("<table> <tr> <th> subject</th> <th>Totalmarks </th> <th>Obtainedmarks</th><th>Percentage</th> </tr> <tr> <td>"
+// + sub_1 + "</td> <td>"+ marks +"</td> <td>"+subMarks1+"</td> <td" +percentage1+"</tr><tr> <td>"+sub_2+ "</td><td>"+marks+"</td><td>"
+// +subMarks2+ "</td> <td>"+ percentage2+ "</tr> <tr> <td>"+ sub_3 + "</td><td>"+marks+"</td><td>"+subMarks3+"</td><td>"+percentage3+
+// "</tr> <tr> <td>","","</td><td>"+total_marks+ "</td><td>"+obtainedMarks+ "</td><td>"+percentage+"%");
